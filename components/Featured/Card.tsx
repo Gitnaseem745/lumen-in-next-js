a0ded2f5a0fd891fad002a31b866e66dd95b22d8
Naseem Ansari <mrnaseem745@gmail.com>
2024-12-02 00:02:15 +0530

    #featrued card created

---
+++ components/Featured/Card.tsx
@@ -0,0 +1,26 @@
+'use client';
+import CardProps from "@/interfaces/CardProps";
+
+interface ExtendedProps extends CardProps {
+        theme?: string;
+}
+const Card: React.FC<ExtendedProps> = ({theme, name, tag, img}) => {
+  return (
+    <div className="featuredCard">
+        <div className="featuredText">
+            <div className='featuredTextDiv'>
+            <p className='text-xl max-sm:text-sm'>{theme}</p>
+            <h1 className='featuredH1'>{name}</h1>
+            <h1 className='font-bold opacity-50 max-sm:text-xs'>{tag}</h1>
+            </div>
+        </div>
+        <div className="featuredImg">
+            <div className='w-[90%] h-[90%]'>
+            <img src={img} className='w-full h-full object-cover'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
