47a4998cbc78788be5fd22b288eff05ec34f5375
Naseem Ansari <mrnaseem745@gmail.com>
2024-12-02 13:46:13 +0530

    #css added for more compoents

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)", // Dynamically set background color
+        foreground: "var(--foreground)", // Dynamically set foreground color
+      },
+      animation: {
+        shimmer: "shimmer 2s linear infinite", // Custom shimmer animation
+      },
+      keyframes: {
+        shimmer: {
+          from: { backgroundPosition: "0 0" }, // Start position for the shimmer
+          to: { backgroundPosition: "-200% 0" }, // End position for the shimmer
+        },
       },
     },
     fontFamily: {
-        'averta': ['averta cv w10', ''],
-      },
+      averta: ["averta cv w10", "sans-serif"], // Added sans-serif as fallback
+    },
   },
   plugins: [],
 } satisfies Config;
