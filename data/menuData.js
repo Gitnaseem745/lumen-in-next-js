4c8321589caefd685f42baccfcfb1251f2fc4974
Naseem Ansari <mrnaseem745@gmail.com>
2024-12-02 13:46:38 +0530

    #menu button created

---
+++ data/menuData.js
@@ -0,0 +1,41 @@
+export const links = [
+    {
+        title: "Home",
+        href: "/"
+    },
+    {
+        title: "About",
+        href: "/"
+    },
+    {
+        title: "Categories",
+        href: "/"
+    },
+    {
+        title: "Photography",
+        href: "/"
+    },
+    {
+        title: "Connect",
+        href: "/"
+    }
+]
+
+export const footerLinks = [
+    {
+        title: "Facebook",
+        href: "/"
+    },
+    {
+        title: "LinkedIn",
+        href: "/"
+    },
+    {
+        title: "Instagram",
+        href: "/"
+    },
+    {
+        title: "Twitter",
+        href: "/"
+    }
+]
