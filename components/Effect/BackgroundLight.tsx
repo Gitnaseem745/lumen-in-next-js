6f1b950d0f1bdd18cfdf6f96ba08996e2cd83069
Naseem Ansari <mrnaseem745@gmail.com>
2024-12-02 13:50:42 +0530

    #background light effect for single post

---
+++ components/Effect/BackgroundLight.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const BackgroundLight = () => {
+  return (
+    <div id="hero-shape" className="absolute w-[46vw] h-[36vw] right-[50vh] top-[-35vh]">
+    <div id="hero-2"></div>
+    <div id="hero-3"></div>
+    </div>
+  )
+}
+export default BackgroundLight
